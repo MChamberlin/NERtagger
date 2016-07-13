4b18831e40124506e02a55deac8ef63d95eb6ed7
Matt Chamberlin <mchamberlin@gild.com>
2016-07-13 13:39:38 -0700

    push printScore logic into SymbTypeCounter class.

--- src/main/scala/tagger/TaggerEvaluator.scala
+++ src/main/scala/tagger/TaggerEvaluator.scala
@@ -6,6 +6,7 @@ import scala.collection.mutable.HashMap
 
 
 protected class SymbTypeCounter {
+  // TODO: deal with potential DivideByZero errors
   var truePos: Int = 0
   var trueNeg: Int = 0
   var falsePos: Int = 0
@@ -23,19 +24,27 @@ protected class SymbTypeCounter {
     (truePos + trueNeg) / getTotalCounts.toDouble
   }
 
+  def getF1Score: Double = {
+    (2 * getPrecision * getRecall) / (getPrecision + getRecall)
+  }
+
   def getTotalCounts: Int = {
     truePos + falsePos + trueNeg + falseNeg
   }
+
+  def printScores: Unit = {
+    println(s"Accuracy  | $getAccuracy")
+    println(s"Precision | $getPrecision")
+    println(s"Recall    | $getRecall")
+    println(s"F1-Score  | $getF1Score")
+  }
+
 }
 
 class TaggerEvaluator(tagger: Tagger) {
-  var truePos: Int = 0
-  var trueNeg: Int = 0
-  var falsePos: Int = 0
-  var falseNeg: Int = 0
+  val totalCounter = new SymbTypeCounter
 
   def collectCounts(devDoc: Document, keyDoc: TaggedCorpus): Unit = {
-    val symbCounts = new HashMap[String,SymbTypeCounter]().withDefaultValue(new SymbTypeCounter)
     devDoc.getSentIter.zip(keyDoc.getSentIter).foreach{ case (sent, trueTags) =>
       tagger.getSentenceTags(sent.toList).zip(trueTags.toList).foreach{ case (predTag,trueTag) =>
         // TODO: handle multiple classes using the SymbTypeCounts class
@@ -44,40 +53,24 @@ class TaggerEvaluator(tagger: Tagger) {
         }
         if (keyDoc.posSymbSet(trueTag.symb)) {
           if (predTag.symb == trueTag.symb) {
-            truePos += 1
+            totalCounter.truePos += 1
           } else {
-            falseNeg += 1
+            totalCounter.falseNeg += 1
           }
         } else {
           if (predTag.symb == trueTag.symb) {
-            trueNeg += 1
+            totalCounter.trueNeg += 1
           } else {
-            falsePos += 1
+            totalCounter.falsePos += 1
           }
         }
       }
     }
   }
 
-  def printScores: Unit = {
-    // TODO: deal with potential DivideByZero errors
-    val accuracy = (truePos + trueNeg) / (truePos + trueNeg + falsePos + falseNeg).toDouble
-    val precision = truePos / (truePos + falsePos).toDouble
-    val recall = truePos / (truePos + falseNeg).toDouble
-    val f1Score = (2 * precision * recall) / (precision + recall)
-    println(s"Accuracy  | ${accuracy}")
-    println(s"Precision | ${precision}")
-    println(s"Recall    | ${recall}")
-    println(s"F1-Score  | ${f1Score}")
-  }
-
   def score(devDoc: Document, keyDoc: TaggedCorpus): Unit = {
-    truePos = 0
-    trueNeg = 0
-    falsePos = 0
-    falseNeg = 0
     collectCounts(devDoc, keyDoc)
-    printScores
+    totalCounter.printScores
   }
 
 }

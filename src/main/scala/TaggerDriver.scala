161710948699b5aa49d2d39bdcec479ee95de14f
Matt Chamberlin <mchamberlin@gild.com>
2016-07-13 21:41:38 -0700

    add option parsing for training corpus and ngram count

--- src/main/scala/TaggerDriver.scala
+++ src/main/scala/TaggerDriver.scala
@@ -7,13 +7,12 @@ import util._
 object TaggerDriver extends App {
   // TODO: add commands to train, score, and tag separately using train/dev/key args
   case class Config(outFile: File = new File("."),
-                    trainingCorpus: TaggedCorpus = WikiTrainingCorpus,
+                    corpus: TaggedCorpus = WikiTrainingCorpus,
+                    keyDoc: TaggedCorpus = WikiKey,
+                    devDoc: Document = WikiDev,
                     rareThreshold: Int = 5,
                     maxNGramSize: Int = 3,
-                    preprocessor: Preprocessor = PatternPreprocessor,
-                    devDoc: Document = WikiDev,
-                    keyDoc: TaggedCorpus = WikiKey
-                     )
+                    preprocessor: Preprocessor = PatternPreprocessor)
 
   val parser = new OptionParser[Config]("NERTagger") {
     head("NER Tagger", "1.0")
@@ -25,12 +24,34 @@ object TaggerDriver extends App {
         else failure(s"${x.getPath} is not writeable")).
       text("output file name")
 
+    opt[String]('c', "corpus").optional().valueName("{wiki|gene}").
+      validate( x =>
+        if (x == "gene" | x == "wiki") success
+        else failure("Corpus must be one of {wiki|gene}")).
+      action( (x, c) => x match {
+        case "wiki" =>
+          c.copy(corpus = WikiTrainingCorpus)
+          c.copy(devDoc = WikiDev)
+          c.copy(keyDoc = WikiKey)
+        case "gene" =>
+          c.copy(corpus = GeneTrainingCorpus)
+          c.copy(devDoc = GeneDev)
+          c.copy(keyDoc = GeneKey)}).
+      text("training corpus; must be one of {wiki|gene}")
+
     opt[Int]('r', "rare").optional().valueName("<int>").
       action( (x, c) => c.copy(rareThreshold = x) ).
       validate( x =>
-      if (x > 0) success
-      else failure(s"Rare theshold $x must be > 0")).
+        if (x > 0) success
+        else failure(s"Rare theshold $x must be > 0")).
       text("rare count threshold")
+
+    opt[Int]('n',"ngram").optional().valueName("<int>").
+      action( (x, c) => c.copy(maxNGramSize = x) ).
+      validate( x =>
+        if (x == 3) success
+        else failure(s"Only n = 3 supported currently")).
+      text("max n-gram size")
 
   }
 
@@ -38,7 +59,7 @@ object TaggerDriver extends App {
   parser.parse(args, Config()).map { config =>
     val tagger = new Tagger(config.rareThreshold, config.maxNGramSize, config.preprocessor)
     println(s"Training tagger...")
-    tagger.train(config.trainingCorpus)
+    tagger.train(config.corpus)
     println(s"Writing tagged sentences...")
     tagger.writeDocumentTags(config.devDoc, config.outFile)
     println(s"Tagged sentences written to ${config.outFile.getName}")

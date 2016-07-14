8fbf4ee3d1277af34aaaed2f1da03c337f4f93ea
Matt Chamberlin <mchamberlin@gild.com>
2016-07-13 21:01:41 -0700

    push getSentIter logic into abstract Document class

--- src/main/scala/util/Document.scala
+++ src/main/scala/util/Document.scala
@@ -4,16 +4,15 @@ import scala.io.Source
 
 
 abstract class Document {
-  def getSentIter: SentIter
-}
-
-object GeneDev extends Document {
-  val filename = "/genetag.dev.txt"
+  val filename: String
 
   def getSentIter: SentIter = {
     val source = Source.fromInputStream(getClass.getResourceAsStream(filename))
     for (line <- source.reset().getLines() if line.trim.nonEmpty)
-      yield line.split(" ").toIterator
+    yield line.split(" ").toIterator
   }
+}
 
+object GeneDev extends Document {
+  val filename = "/genetag.dev.txt"
 }
